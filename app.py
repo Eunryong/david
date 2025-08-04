421b344d5e135968e2ea36a4cf362b167526b7f1
Eunryong <86572427+Eunryong@users.noreply.github.com>
2025-08-04 17:41:34 +0900

    feat: test2

--- app.py
+++ app.py
@@ -68,5 +68,9 @@ def home():
 def menu():
     return render_template('menu.html')
 
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', 8080, debug=True)
